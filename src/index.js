80624c988ebb52f417e3d6e0aaa339ca45c63c4e
NOBATGELDI GELDIMAMMEDOV <nobatgeldi@covisart.com>
2024-04-08 23:32:59 +0300

    removed some unused packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
