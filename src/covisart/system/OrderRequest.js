be9f43a3e01baeb345c0ec17235ce454feec2a34
NOBATGELDI <nobatgeldi@covisart.com>
2025-08-12 03:59:47 +0300

    contry dropdown bug fixed

--- src/covisart/system/OrderRequest.js
+++ src/covisart/system/OrderRequest.js
@@ -1,29 +1,7 @@
 import Axios from "axios";
 
-export const LocalOrder = async  (state) => {
-  const data = {
-      email: state.email,
-      name: state.name,
-      country: state.country,
-      description: state.message,
-      size: state.size,
-      phone: state.phone,
-      accessory: state.accessory,
-      color: state.color,
-      reservedObjects: [
-        "string"
-      ]
-    }
-  const resp = await Axios.post("http://localhost:5290/api/Orders/", data,)
-  .then((res) => {
-      console.log(res)
-  })
-  .catch((err) => { 
-      console.log(err)
-  });
-};
 export const Order = async  (state) => {
-    const data = {
+  const data = {
         email: state.email,
         name: state.name,
         country: state.country,
@@ -36,12 +14,12 @@ export const Order = async  (state) => {
           "string"
         ]
       }
-    const resp = await Axios.post("https://ngsmarket.azurewebsites.net/api/Orders/", data,)
-    .then((res) => {
-        console.log(res)
+  const resp = await Axios.post("https://ngsmarket.azurewebsites.net/api/Orders/", data)
+    .then((resp) => {
+      console.log(resp);
     })
-    .catch((err) => { 
-        console.log(err)
+    .catch((err) => {
+      console.log(err);
     });
 };
 
